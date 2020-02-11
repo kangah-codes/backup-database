22a600dc0e36b68f5015172050b3ac85aa8837dd
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2020-02-11 01:18:30 +0000

    Update backup.py

--- backup.py
+++ backup.py
@@ -10,6 +10,7 @@ import slack
 client = slack.WebClient(token='xoxb-919881679474-922523632689-avtN5jmEihuoieHdIRd0cb02')
 
 # setting a log file to log all data
+logging.basicConfig(filename='backup.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.basicConfig(level=logging.INFO)
 
 def backup(url):
@@ -25,10 +26,9 @@ def backup(url):
 	port = database_url.port
 	db = database_url.path[1:]
 
-	logging.info(f"Parsed database url at {datetime.datetime.now()}")
+	logging.info("Parsed database url")
 
 	# set an output directory for the database
-	# output_file = os.path.abspath(os.path.join(os.path.curdir, '/'))
 	output_file = f'{os.getcwd()}/backup'
 
 	# assert os.path.isdir(output_file), 'Directory does not exist'
@@ -41,24 +41,23 @@ def backup(url):
 	
 	os.system(f"zip -r {zip_name} backup/heroku_dczdt2sx")
 	
-	logging.info(f"Compressed database backup at {datetime.datetime.now()}")
+	logging.info("Compressed database backup")
 
 	# log the output of the command
 	logging.info(f"Backed up database at {datetime.datetime.now()}")
 	response = client.files_upload(
 		channels="#backups",
-		file=f"{os.getcwd()}/backup/{zip_name}"
+		file=f"{os.getcwd()}/{zip_name}"
 	)
 
 	assert response['ok'], "Could not send file"
 
-	logging.info(f"Sent backup to slack channel at {datetime.datetime.now()}")
+	logging.info("Sent backup to slack channel")
 
 
 
 if __name__ == '__main__':
 	try:
 		backup('mongodb://heroku_dczdt2sx:ifhpheh6mkgeh9affb2ttftn52@ds147446.mlab.com:47446/heroku_dczdt2sx')
-		#backup('postgres://postgres:ilovekwame1@127.0.0.1:5432/medic_mall')
 	except AssertionError as e:
 		logging.error(e)
